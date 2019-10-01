77131ea9ee13911172a88d99ec3908ba55a8566f
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-10-01 12:47:49 +0200

    Uploading first version

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import HeaderContainer from './components/HeaderContainer';
+import PageHeading from './components/PageHeading';
+import CardContainer from './components/CardContainer';
+
+
+function App() {
+  return (
+    <body>
+    <HeaderContainer/>
+    <PageHeading/>
+    <CardContainer/>
+    </body>
+    );
+}
+
+export default App;
